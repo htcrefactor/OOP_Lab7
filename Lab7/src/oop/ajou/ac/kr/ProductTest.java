648a743063a7a3bec1c548cc451a1f876b7ad057
KimHyungChan <hj1003hj@ajou.ac.kr>
2017-05-02 00:47:01 +0900

    -initial version of "ProductTest.java"

--- Lab7/src/oop/ajou/ac/kr/ProductTest.java
+++ Lab7/src/oop/ajou/ac/kr/ProductTest.java
@@ -1,35 +1,60 @@
 package oop.ajou.ac.kr;
 
 public class ProductTest {
-	ProductTest(){};
-	
-	public static void main(String args[]){
-		SmartTV tv=new SmartTV();
-		Laptop l=new Laptop();
-		Toaster t=new Toaster();
-		
-		ProductTest pt=new ProductTest();
-		
+	ProductTest() {
+	};
+
+	public static void main(String args[]) {
+		SmartTV tv = new SmartTV();
+		System.out.println();
+
+		Laptop l = new Laptop();
+		System.out.println();
+
+		Toaster t = new Toaster();
+		System.out.println();
+
+		ProductTest pt = new ProductTest();
+
 		pt.testProduct(tv);
+		System.out.println();
+
+		pt.testProduct(l);
+		System.out.println();
+
+		pt.testProduct(t);
+		System.out.println();
+
+		pt.testDataStorage(l);
+		System.out.println();
+
+		pt.testNetworked(tv);
+		System.out.println();
+
+		pt.testNetworked(l);
+		System.out.println();
+
+		pt.testCooker(t);
+		System.out.println();
 	}
-	
-	public void testProduct(Product p){
+
+	public void testProduct(Product p) {
 		p.setPrice(153);
-		System.out.println("Price: "+p.getPrice());
-		System.out.println("Name: "+p.getName());
+		System.out.println("Price: " + p.getPrice());
+		System.out.println("Name: " + p.getName());
 	}
-	
-	public void testDataStorage(DataStorage d){
-		System.out.println("Free Capacity: "+d.getFreeCapacity());
+
+	public void testDataStorage(DataStorage d) {
+		System.out.println("Free Capacity: " + d.getFreeCapacity());
 		d.format();
 	}
-	
-	public void testNetworked(Networked n){
-		System.out.println("This is connected? "+n.isConnected());
-		System.out.println("Max Speed: "+n.maxSpeed());
+
+	public void testNetworked(Networked n) {
+		System.out.println("This is connected?\n" + n.isConnected());
+		System.out.println("Max Speed: " + n.maxSpeed());
 	}
-	
-	public void testCooker(Cooker c){
-		System.out.println("Did you make toast? "+c.prepareFood());
+
+	public void testCooker(Cooker c) {
+		System.out.println("Did you make toast? \n" + c.prepareFood());
 	}
 }
