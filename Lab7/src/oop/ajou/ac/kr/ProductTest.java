f9b0db5a323d9263f61cb4536bedae37c449929f
kyw <kyw@DESKTOP-LOAQF5P>
2017-05-02 00:20:02 +0900

    -initial version of "ProductTest.java"

--- Lab7/src/oop/ajou/ac/kr/ProductTest.java
+++ Lab7/src/oop/ajou/ac/kr/ProductTest.java
@@ -4,10 +4,32 @@ public class ProductTest {
 	ProductTest(){};
 	
 	public static void main(String args[]){
-	
+		SmartTV tv=new SmartTV();
+		Laptop l=new Laptop();
+		Toaster t=new Toaster();
+		
+		ProductTest pt=new ProductTest();
+		
+		pt.testProduct(tv);
 	}
 	
 	public void testProduct(Product p){
-		
+		p.setPrice(153);
+		System.out.println("Price: "+p.getPrice());
+		System.out.println("Name: "+p.getName());
+	}
+	
+	public void testDataStorage(DataStorage d){
+		System.out.println("Free Capacity: "+d.getFreeCapacity());
+		d.format();
+	}
+	
+	public void testNetworked(Networked n){
+		System.out.println("This is connected? "+n.isConnected());
+		System.out.println("Max Speed: "+n.maxSpeed());
+	}
+	
+	public void testCooker(Cooker c){
+		System.out.println("Did you make toast? "+c.prepareFood());
 	}
 }
