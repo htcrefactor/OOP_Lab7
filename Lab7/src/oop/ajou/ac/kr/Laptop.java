a4bce320606b06b8b82c0f11fc7be0e0b35b40d0
KimYoungWoong <djehoaos@naver.com>
2017-05-01 11:51:43 +0900

    -initial version of "Laptop.java"

--- Lab7/src/oop/ajou/ac/kr/Laptop.java
+++ Lab7/src/oop/ajou/ac/kr/Laptop.java
@@ -1,5 +1,58 @@
 package oop.ajou.ac.kr;
 
-public class Laptop {
+public class Laptop extends Product implements Networked {
+	
+	double totalCapacity;
+	double usedCapacity;
+	
+	Laptop(){
+		System.out.println("This is Labtop");
+	}
+	
+	double getFreeCapacity(){
+		return 0;
+	}
+	
+	
+	public void format(){
+		System.out.println("Start formating.");
+	}
+	
+	public boolean isConnected(){
+		return true;
+	}
+	
+	public double maxSpeed(){
+		return 134.1;
+	}
 
+	public double getTotalCapacity() {
+		return totalCapacity;
+	}
+
+	public void setTotalCapacity(double totalCapacity) {
+		if(totalCapacity>0){
+			this.totalCapacity = totalCapacity;
+		}
+		
+		else
+			this.totalCapacity=0;
+	}
+
+	public double getUsedCapacity() {
+		return usedCapacity;
+	}
+
+	public void setUsedCapacity(double usedCapacity) {
+		if(totalCapacity>0){
+			this.usedCapacity = usedCapacity;
+		}
+		
+		else
+			this.usedCapacity=0;
+	}
+	
+	public String getName(){
+		return "Gold Laptop";
+	}
 }
