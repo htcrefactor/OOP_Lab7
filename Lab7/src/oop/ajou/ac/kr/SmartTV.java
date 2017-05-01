3f2ed43fb8ed783dc1755a24e91f71ef417161bd
KimHyungChan <hj1003hj@ajou.ac.kr>
2017-05-01 19:37:29 +0900

    -initial version of "SmartTV.java"

--- Lab7/src/oop/ajou/ac/kr/SmartTV.java
+++ Lab7/src/oop/ajou/ac/kr/SmartTV.java
@@ -1,30 +1,33 @@
 package oop.ajou.ac.kr;
 
-public class SmartTV {
+public class SmartTV extends Product implements Networked{
 	private int channel;
 	
-	public void SmartTV()
+	SmartTV()
 	{
-		System.out.println("We create the SmartTV");
+		System.out.println("We create the SmartTV method");
 	}
-	public boolean isConneected()
-	{
-		return true;
-	}
-	
+
 	public double maxSpeed()
 	{
 		return 158.7;
 	}
 	
-	public String getName();
+	public String getName()
+	{
+		return "This is SmartTV!!";
+	}
 	
 	public int getChannel() {
 		return channel;
 	}
 	public void setChannel(int channel) {
-		this.channel = channel;
+		if (channel<0) this.channel=0;
+		else this.channel = channel;
 	}
-	
+
+	public boolean isConnected() {
+		return true;
+	}
 	
 }
